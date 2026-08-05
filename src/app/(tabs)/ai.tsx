e7519a5ba15d4906adc2fbd6489f5589093fb2c2
muhammad fahim <suleimanmuhammadfahim@gmail.com>
2026-08-05 14:57:12 +0100

    Ai screen

--- src/app/(tabs)/ai.tsx
+++ src/app/(tabs)/ai.tsx
@@ -41,9 +41,119 @@ export default function AiScreen() {
         </View>
         </View>
 
-        <View style={{maxWidth:'70%', backgroundColor:'#00A86B', alignSelf:'flex-start'}}></View>
-        <View></View>
-        <View></View>
+        <View style={{maxWidth:'70%', backgroundColor:'#FFFFFF', alignSelf:'flex-start', marginTop:20, shadowColor: "#000",shadowOffset: {
+  width: 0,
+  height: 2,
+},
+shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 8 , padding:15, gap:4, borderRadius:12}}>
+          <Text>Hi john! 👋</Text>
+          <Text>How can i help you grow </Text>
+          <Text>your business today?</Text>
+        </View>
+        <View style={{maxWidth:'70%', backgroundColor:'#00A86B', alignSelf:'flex-end',marginTop:25, padding:15, gap:4, borderRadius:12}}>
+           <Text style={{color:'white'}}>How much profit did i</Text>
+          <Text style={{color:'white'}}>make this month?</Text>
+        </View>
+        <View style={{maxWidth:'80%', backgroundColor:'#FFFFFF', alignSelf:'flex-start', marginTop:25, shadowColor: "#000",shadowOffset: {
+  width: 0,
+  height: 2,
+},
+shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 8 , padding:18, gap:4, borderRadius:12}}>
+
+            <Text style={{fontSize:15, fontWeight:"500",}}>Your profit for july 2026 is</Text>
+            <Text style={{fontSize:20, fontWeight:"bold",}}> $8,430.00</Text>
+            <Text style={{ color: "#22C55E", fontSize:12, marginTop:8 }}>
+    ▲10.3%
+    <Text style={{ color: "#64748B" }}> vs june 2026</Text>
+  </Text>
+
+  <TouchableOpacity style={{backgroundColor:'white', borderWidth:1, borderColor:"#00A86B", marginTop:20, padding:10, borderRadius:12, justifyContent:'center', alignItems:"center", shadowColor: "#000",shadowOffset: {
+  width: 0,
+  height: 2,
+},
+shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 2 , }}>
+      <Text style={{color:"#00A86B"}}>View detailed report</Text>
+  </TouchableOpacity>
+        </View>
+        <View style={{flexDirection:'row', justifyContent:'space-between', gap:4, marginTop:40}}>
+
+              <TouchableOpacity style={{shadowColor: "#000",shadowOffset: {},shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 2, backgroundColor:'white',borderRadius:20,justifyContent:'center', alignItems:"center", padding:10}}>
+  <Text style={{fontSize:12, fontWeight:'bold'}}>Generate invoice</Text>
+</TouchableOpacity>
+              <TouchableOpacity style={{shadowColor: "#000",shadowOffset: {},shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 2, backgroundColor:'white',borderRadius:20,justifyContent:'center', alignItems:"center", padding:10}}>
+  <Text style={{fontSize:12, fontWeight:'bold'}}>Analyze sales</Text>
+</TouchableOpacity>
+              <TouchableOpacity style={{shadowColor: "#000",shadowOffset: {},shadowOpacity: 0.05,
+shadowRadius: 6,
+elevation: 2, backgroundColor:'white',borderRadius:20,justifyContent:'center', alignItems:"center", padding:10}}>
+  <Text style={{fontSize:12, fontWeight:'bold'}}  >write ad copy</Text>
+</TouchableOpacity>
+        </View>
+
+     <View
+  style={{
+    marginTop: 60,
+    backgroundColor: "white",
+    borderRadius: 20,
+    height: 56,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 15,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 6,
+    elevation: 2,
+  }}
+>
+  <TextInput
+    placeholder="Ask anything..."
+    placeholderTextColor="gray"
+    style={{
+      flex: 1,
+      fontSize: 15,
+      color:'black'
+    }}
+  />
+
+  <MaterialCommunityIcons
+    name="microphone"
+    size={22}
+    color="gray"
+    style={{ marginRight: 12 }}
+  />
+
+  <View
+    style={{
+      width: 34,
+      height: 34,
+      borderRadius: 17,
+      backgroundColor: "#E2E8F0",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <MaterialCommunityIcons
+      name="arrow-right"
+      size={18}
+      color="#64748B"
+    />
+  </View>
+</View>
     
   </ScrollView>
    </SafeAreaView>
--- src/app/(tabs)/customers.tsx
+++ src/app/(tabs)/customers.tsx
@@ -1,9 +1,168 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, FlatList, TextInput,  SafeAreaView,Image } from 'react-native';
+import { useState } from 'react';
 
+
+const customersData = [
+  { id: '1', name: 'Sarah johnson', email: 'sarah@gmail.com', amount:'$620', percentage: '70%',icon: require('../../../assets/images/mine.png'),bgColor: '#EFF6FF' },
+  { id: '2', name: 'Micheal Brown', email: 'micheal@gmail.com', amount:'$620', percentage: '60%',icon: require('../../../assets/images/mine.png'), bgColor: '#EFF6FF' },
+  { id: '3', name: 'David Wilson', email: 'david@gmail.com', amount:'$620', percentage: '68%',icon: require('../../../assets/images/mine.png'), bgColor: '#EFF6FF' },
+  { id: '4', name: 'Emily Davis', email: 'emily@gmail.com',amount:'$620', percentage: '40%',icon: require('../../../assets/images/mine.png'),bgColor: '#EFF6FF' },
+  { id: '5', name: 'James Anderson', email: 'james@gmail.com', amount:'$620', percentage: '55%',icon: require('../../../assets/images/mine.png'), bgColor: '#EFF6FF' },
+];
 export default function CustomersScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredData, setFilteredData] = useState(customersData);
+  const handleSearch = (text) => {
+    setSearchQuery(text);
+
+     if (text) {
+      const newData = customersData.filter((item) => {
+        const itemData = item.name.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredData(newData);
+    } else {
+      setFilteredData(customersData);
+    }
+  }
   return (
-    <View>
-      <Text>Sign In</Text>
+   <SafeAreaView style={{ flex: 1, backgroundColor: "#F8FAFC" }}>
+      <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style ={styles.headertext}>Customers</Text>
+          <TextInput
+        style={styles.searchInput}
+        placeholder="Search customers"
+        placeholderTextColor="#9CA3AF"
+        value={searchQuery}
+        onChangeText={handleSearch}
+        clearButtonMode="while-editing" // iOS only clear button
+      />
+      </View>
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ paddingBottom: 20 }}
+        renderItem={({ item }) => {
+           return(
+
+          <View style={styles.card}>
+            <View style={styles.leftSection}>
+              <View style={[styles.iconPlaceholder, , { backgroundColor: item.bgColor }]}>
+             <Image
+              source={item.icon}
+              style={{
+                width: 45,
+                height: 45,
+                borderRadius: 22.5,
+              }}
+            />
+                 </View> 
+              <View>
+                <Text style={styles.subjectName}>{item.name}</Text>
+                <Text style={styles.topicsText}>{item.email}</Text>
+                <Text style={styles.topicsText}>Total Spent:{item.amount}</Text>
+              </View>
+            </View>
+
+            <View style={styles.rightSection}>
+              <Text style={styles.percentageText}>{item.percentage}</Text>
+            </View>
+          </View>
+        );
+      }}
+      />
     </View>
+       </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F8F9FA',
+    paddingHorizontal: 16,
+    paddingTop:60
+  },
+  header:{
+    flexDirection:'column',
+    alignItems:'flex-start',
+    justifyContent:'space-between'
+  },
+  headertext:{
+    fontSize:28,
+    fontWeight:'semibold',
+    paddingBottom:16
+  },
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#FFFFFF',
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 12,
+  },
+  leftSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+  },
+  iconPlaceholder: {
+    width: 40,
+    height: 40,
+    borderRadius: 8,
+    backgroundColor: '#F0F8FF',
+  },
+  subjectName: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#1A1A1A',
+  },
+  topicsText: {
+    fontSize: 12,
+    color: '#7A7A7A',
+    marginTop: 2,
+  },
+  rightSection: {
+    alignItems: 'flex-end',
+    gap: 12,
+  },
+  progressBarBg: {
+    width: 60,
+    height: 6,
+    backgroundColor: '#EBEBEB',
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+  progressBarFill: {
+    height: '100%',
+    backgroundColor: '#3B82F6',
+    borderRadius: 3,
+  },
+  percentageText: {
+    fontSize: 12,
+    color: '#7A7A7A',
+  },
+  searchInput: {
+    height: 40,
+    width:'100%',
+    borderColor: '#ccc',
+    borderRadius: 8,
+    marginBottom: 20,
+    backgroundColor:'#FFFFFF',
+    justifyContent:'flex-start',
+    alignItems:'flex-start',
+    color:'#1F2937',
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 4 }, 
+   
+    shadowOpacity: 0.06, 
+
+    shadowRadius: 8, 
+
+    elevation: 4, 
+  },
+});
+
